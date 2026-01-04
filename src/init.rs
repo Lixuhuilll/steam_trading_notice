43ec8357c472f882286153e46a1ca21b43596340
lixuhuilll <676824363@qq.com>
2026-01-04 20:10:16 +0800

    实现邮件服务器初始化代码

--- src/init.rs
+++ src/init.rs
@@ -1,4 +1,11 @@
+use crate::init::TlsMode::{STARTTLS, TLS};
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::transport::smtp::{SUBMISSION_PORT, SUBMISSIONS_PORT};
+use lettre::{AsyncSmtpTransport, Tokio1Executor};
+use std::time::Duration;
+use tracing::{info, instrument, warn};
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 
 pub fn log_init() -> Result<Vec<WorkerGuard>, Box<dyn std::error::Error>> {
@@ -32,10 +39,93 @@ pub fn log_init() -> Result<Vec<WorkerGuard>, Box<dyn std::error::Error>> {
 
     let subscriber = tracing_subscriber::registry()
         .with(console_layer)
-        .with(file_layer);
+        .with(file_layer)
+        .with(LevelFilter::INFO);
 
     tracing::subscriber::set_global_default(subscriber)?;
 
     // 所有的 WorkerGuard 都必须返回到 main 函数，妥善保管直至程序结束
     Ok(vec![console_guard, file_guard])
+}
+
+#[derive(Debug)]
+enum TlsMode {
+    TLS,
+    STARTTLS,
+}
+
+#[instrument(skip(smtp_password))]
+pub async fn smtp_init(
+    smtp_host: &str,
+    smtp_port: u16,
+    smtp_username: &str,
+    smtp_password: &str,
+    smtp_timeout: u64,
+) -> Result<AsyncSmtpTransport<Tokio1Executor>, Box<dyn std::error::Error>> {
+    if smtp_host.len() == 0 || smtp_username.len() == 0 || smtp_password.len() == 0 {
+        let err = format!(
+            "smtp 参数异常，主机名、用户名、密码必须全部具备，host={}，username={}，password.len={}",
+            smtp_host,
+            smtp_username,
+            smtp_password.len()
+        );
+        return Err(err.into());
+    }
+
+    for mode in [TLS, STARTTLS] {
+        let mut smtp_port = smtp_port;
+
+        let mut transport_build = match mode {
+            TLS => {
+                if smtp_port == 0 {
+                    smtp_port = SUBMISSIONS_PORT;
+                }
+                AsyncSmtpTransport::<Tokio1Executor>::relay(&smtp_host)?.port(smtp_port)
+            }
+            STARTTLS => {
+                if smtp_port == 0 {
+                    smtp_port = SUBMISSION_PORT;
+                }
+                AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(&smtp_host)?.port(smtp_port)
+            }
+        };
+
+        info!(
+            "尝试与 SMTP 服务器（{}:{}）建立 {:?} 加密连接",
+            smtp_host, smtp_port, mode
+        );
+
+        if smtp_timeout > 0 {
+            transport_build = transport_build.timeout(Some(Duration::from_secs(smtp_timeout)));
+        }
+
+        let transport: AsyncSmtpTransport<Tokio1Executor> = transport_build
+            .credentials(Credentials::new(
+                smtp_username.to_owned(),
+                smtp_password.to_owned(),
+            ))
+            .build();
+
+        match transport.test_connection().await {
+            Ok(true) => {
+                info!(
+                    "成功与 SMTP 服务器（{}:{}）建立 {:?} 加密连接。",
+                    smtp_host, smtp_port, mode
+                );
+
+                return Ok(transport);
+            }
+            Ok(false) => {
+                warn!(
+                    "无法与 SMTP 服务器（{}:{}）建立 {:?} 加密连接。 没有更多相关信息了。",
+                    smtp_host, smtp_port, mode
+                );
+            }
+            Err(err) => {
+                warn!(err = %err, "无法与 SMTP 服务器（{}:{}）建立 {:?} 加密连接。", smtp_host, smtp_port, mode);
+            }
+        };
+    }
+
+    Err("SMTP 服务器连接失败".into())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use tracing::info;
+use tracing::{error, info};
 
 mod init;
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    init::smtp_init("test", 0, "test", "00", 5).await?;
+
+    // 优雅退出
+    tokio::signal::ctrl_c().await?;
+
+    Ok(())
+}
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 初始化日志
     let _guards = init::log_init()?;
     info!("日志系统初始化完成");
+
+    // 全局错误日志记录
+    if let Err(err) = run().await {
+        // 全局错误日志记录
+        error!("应用程序发生致命错误: {}", err);
+        // 抛出错误
+        return Err(err);
+    }
 
     Ok(())
 }
